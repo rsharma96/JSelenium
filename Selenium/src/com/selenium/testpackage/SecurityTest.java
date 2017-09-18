e28ae5bd94da0517664564e08ad89bc871391436
rsharma96 <30986019+rsharma96@users.noreply.github.com>
2017-09-18 19:26:55 +0530

    Added Scanner Class to Test Package

--- Selenium/src/com/selenium/testpackage/SecurityTest.java
+++ Selenium/src/com/selenium/testpackage/SecurityTest.java
@@ -2,6 +2,8 @@ package com.selenium.testpackage;
 
 import com.selenium.utility.Security;
 
+import java.util.Scanner;
+
 /**
  * Created by gaurnitai on 2017-09-18.
  */
@@ -9,11 +11,19 @@ public class SecurityTest {
 
     public static void main(String args[]){
 
-        String uname = Security.encryt("rsharma96");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String pwd = sc.nextLine();
+
+        String uname = Security.encryt(name);
         System.out.println(uname);
 
         Security.decryt(uname);
         System.out.println(Security.decryt(uname));
+
+
+        String password = Security.decryt(Security.encryt(pwd));
+        System.out.println(password);
 
 
     }
