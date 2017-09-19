a0fdc55bda5ecb3a055a96ae648bf2e26e2c7d00
rsharma96 <30986019+rsharma96@users.noreply.github.com>
2017-09-19 12:50:53 +0530

    Added CaptureScreen class to Utility package

---
+++ Selenium/src/com/selenium/utility/CaptureScreenshot.java
@@ -0,0 +1,19 @@
+package com.selenium.utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by gaurnitai on 2017-09-19.
+ */
+public class CaptureScreenshot {
+
+    public void takeScreenshot(){
+
+
+
+
+    }
+
+
+}
