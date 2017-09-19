b65454a47f56531e4b7b566eecdce669036975e5
rsharma96 <30986019+rsharma96@users.noreply.github.com>
2017-09-19 14:15:37 +0530

    Added screenshot capture code

--- Selenium/src/com/selenium/utility/CaptureScreenshot.java
+++ Selenium/src/com/selenium/utility/CaptureScreenshot.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -27,7 +26,7 @@ public class CaptureScreenshot {
 
         FileUtils.copyFile(scr,
                 new File("/Users/gaurnitai/Documents/SeleniumJava/" +
-                        "IDEAJava/JSelenium/screenshots/"+System.currentTimeMillis()+".jpeg"));
+                        "IDEAJava/JSelenium/screenshots/" + System.currentTimeMillis() + ".jpeg"));
 
 
     }
